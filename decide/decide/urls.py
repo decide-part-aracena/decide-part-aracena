a467329245b73654f70727db9babcb7c7ed513e0
egc-ruben <rsuarezdavid@gmail.com>
2022-11-03 01:36:54 +0100

    feature #10: Realizado frontend de administracion de censo

--- decide/decide/urls.py
+++ decide/decide/urls.py
@@ -17,14 +17,19 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_swagger.views import get_swagger_view
-from base import views
+from base import views as v1
+from census import views as v2
 
 schema_view = get_swagger_view(title='Decide API')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name = 'home'),
+    path('', v1.home, name = 'home'),
     path('doc/', schema_view),
+    path('censo/', v2.listar_censos, name = 'censo'),
+    path('censo/crear', v2.crear_censo, name = 'crear_censo'),
+    path('censo/<int:votacion_id>', v2.censo_details, name = 'censo_details'),
+    path('borrar/censo/<int:votacion_id>', v2.borrar_censo, name = 'borrar_censo'),
     path('gateway/', include('gateway.urls')),
 ]
 
