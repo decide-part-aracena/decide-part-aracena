1906647a97667bc3639fa62fc408ae1928c92eae
egc-ruben <rsuarezdavid@gmail.com>
2022-11-12 21:03:11 +0100

    feature #10: agregado template del fontend a mi funcionalidad

--- decide/census/urls.py
+++ decide/census/urls.py
@@ -1,13 +1,7 @@
 from django.urls import path, include
 from . import views
-from census import views
-
 
 urlpatterns = [
     path('', views.CensusCreate.as_view(), name='census_create'),
     path('<int:voting_id>/', views.CensusDetail.as_view(), name='census_detail'),
-    path('censo/', views.listar_censos, name = 'censo'),
-    path('censo/crear', views.crear_censo, name = 'crear_censo'),
-    path('censo/<int:votacion_id>', views.censo_details, name = 'censo_details'),
-    path('borrar/censo/<int:votacion_id>', views.borrar_censo, name = 'borrar_censo'),
 ]
