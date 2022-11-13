1e82f02d2322b017beae39dbcd14b1dbca6478b0
lau <lausalbra@alum.us.es>
2022-11-13 13:49:28 +0100

    feature

--- decide/decide/urls.py
+++ decide/decide/urls.py
@@ -17,8 +17,7 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_swagger.views import get_swagger_view
-from voting import views
-
+from voting import views as v1
 
 
 schema_view = get_swagger_view(title='Decide API')
@@ -27,13 +26,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('doc/', schema_view),
     path('gateway/', include('gateway.urls')),
-    path('question/', views.listaPreguntas, name='preguntas'),
-    path('question/crear/', views.crearPreguntas, name='crear_preguntas'),
-    path('question/<int:question_id>/', views.showUpdateQuestions, name='showUpdateQuestions'),
-    path('borrar/question/<int:question_id>/', views.borrarPreguntas, name='borrar_preguntas'),
-    path('voting/', views.listarQuestions, name = 'question_list'),
-    path('voting/<int:voting_id>', views.voting_details, name = 'voting_details'),
-    path('voting/crear', views.crear_voting, name = 'crear_voting')
+    path('voting/', v1.listarQuestions, name = 'question_list'),
+    path('voting/<int:voting_id>', v1.voting_details, name = 'voting_details'),
+    path('voting/crear', v1.crear_voting, name = 'crear_voting')
+    
 ]
 
 for module in settings.MODULES:
