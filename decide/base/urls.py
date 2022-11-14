da23e3691c9ffea0d82b4fdac7d35560eb49da28
erngutcon <erngutcon@alum.us.es>
2022-11-14 15:52:37 +0100

    feature #11: Añadido botón para exportar el censo a csv

--- decide/base/urls.py
+++ decide/base/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from django.contrib.auth.decorators import login_required
 from census import views
 from .views import mainView
+from .views import export_csv
 
 urlpatterns = [
     path('', login_required(mainView.as_view()), name='index'),
    # path('import_datadb', views.excel, name='import_datadb'),
+   path('census_exported', export_csv),
 ]
--- decide/base/views.py
+++ decide/base/views.py
@@ -1,6 +1,11 @@
+import csv
 from census.models import Census
 from voting.models import Voting
 from django.views.generic.base import TemplateView
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
 
 class mainView(TemplateView):
     template_name = 'base/index.html'
@@ -20,3 +25,23 @@ class mainView(TemplateView):
         context['finished_votings'] = finished_votings
         context['tally_votings'] = tally_votings
         return context
+
+#Creada para la task -----------------------------------------------------------------
+def export_csv(request):
+
+    queryset = Census.objects.all()
+
+    options = Census._meta
+    fields = [field.name for field in options.fields]
+
+    response = HttpResponse(content_type='text/csv')
+    response['Content_Disposition'] = 'atachment; filename="census.csv"'
+
+    writer = csv.writer(response)
+
+    writer.writerow([options.get_field(field).verbose_name for field in fields])
+
+    for obj in queryset:
+        writer.writerow([getattr(obj, field) for field in fields])
+    
+    return response
