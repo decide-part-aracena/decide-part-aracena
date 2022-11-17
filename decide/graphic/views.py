6625ff97467b58037a7455977e02285321a3e0f7
josgarboh <garciaji700@gmail.com>
2022-11-17 23:23:16 +0100

    feature #14: Muestreo de resultados con gráficas unicolor completo

--- decide/graphic/views.py
+++ decide/graphic/views.py
@@ -9,7 +9,7 @@ from base import mods
 class GraphicView(TemplateView):
     template_name = 'graphic/graphic.html'
 
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         vid = kwargs.get('voting_id', 0)
