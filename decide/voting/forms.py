123aea5a7daf73be7c8c417267f46e5f5dae0e17
lau <lausalbra@alum.us.es>
2022-11-21 00:14:43 +0100

    Solucionando errores de la rama

--- decide/voting/forms.py
+++ decide/voting/forms.py
@@ -1,5 +1,11 @@
+from voting.models import Question
 from django.forms import ModelForm
 from voting.models import Voting
+
+class QuestionForm(ModelForm):
+    class Meta: 
+        model= Question
+        fields = ['desc']
 
 class VotingForm(ModelForm):
     class Meta:
