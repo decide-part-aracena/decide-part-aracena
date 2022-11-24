2687b6701d704708c5a107f5baab63d29097ab8f
noelopdur <noelopdur@alum.us.es>
2022-11-24 17:22:12 +0000

    fix #26: Actualizado local_setting.example.py

--- decide/local_settings.example.py
+++ decide/local_settings.example.py
@@ -36,7 +36,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'decide',
         'USER': 'decide',
-        'PASSWORD': 'decide',
+        'PASSWORD': 'complexpassword',
         'HOST': 'localhost',
         'PORT': '5432',
     }
