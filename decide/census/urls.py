8c6b786da22971df383c029e4f17669196f0f432
erngutcon <erngutcon@alum.us.es>
2022-11-29 19:29:57 +0100

    Correccíon de fix

--- decide/census/urls.py
+++ decide/census/urls.py
@@ -2,12 +2,13 @@ from django.urls import path, include
 from . import views
 from .views import *
 
+
 urlpatterns = [
     path('', views.CensusCreate.as_view(), name='census_create'),
     path('<int:voting_id>/', views.CensusDetail.as_view(), name='census_detail'),
     path('import_datadb', import_datadb, name='import'),
     path('census/', views.listar_censos, name = 'censo'),
-    path('census_exported', views.export_csv),
+    path('census/census_exported', views.export_csv),
     path('census/create', views.crear_censo, name = 'crear_censo'),
     path('census/<int:votacion_id>', views.censo_details, name = 'censo_details'),
     path('delete/census/<int:votacion_id>', views.borrar_censo, name = 'borrar_censo'),
