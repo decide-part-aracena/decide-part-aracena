e85093bdd408e542de353214046ffc0c55251ac9
erngutcon <80253067+erngutcon@users.noreply.github.com>
2022-12-10 13:09:30 +0100

    fix #11: Solución parcial del coverage de Codacy

--- decide/base/views.py
+++ decide/base/views.py
@@ -1,9 +1,6 @@
-import csv
 from census.models import Census
 from voting.models import Voting
 from django.views.generic.base import TemplateView
-from django.shortcuts import render
-from django.http import HttpResponse
 
 
 
