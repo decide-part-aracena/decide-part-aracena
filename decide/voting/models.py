a13ab88c626a00a9bead6c7f697032b414e2a959
alecarnun <alecarnun@alum.us.es>
2022-12-13 21:09:13 +0100

    feat #102: Aplicar la ley d'Hont. Arreglo segun Codacy

--- decide/voting/models.py
+++ decide/voting/models.py
@@ -47,7 +47,8 @@ class Voting(models.Model):
     name = models.CharField(max_length=200)
     desc = models.TextField(blank=True, null=True)
     question = models.ManyToManyField(Question, related_name='votings')
-    typePostproc = models.CharField(max_length=8, help_text='Debe elegir entre IDENTITY o DHONT como método de recuento', choices=(('IDENTITY', 'IDENTITY'), ('DHONT','DHONT')), default='IDENTITY')
+    typePostproc = models.CharField(max_length=8, help_text='Debe elegir entre IDENTITY o DHONT como método de recuento', 
+                        choices=(('IDENTITY', 'IDENTITY'), ('DHONT','DHONT')), default='IDENTITY')
     seats = models.PositiveIntegerField(default=0, help_text='Introduzca el número de escaños a repartir en caso de elegir DHONT')
 
     start_date = models.DateTimeField(blank=True, null=True)
