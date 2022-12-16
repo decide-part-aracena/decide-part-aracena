baadacf47e3ff9394b3c963b161e1e696628b378
Laura <56022111+lausalbra@users.noreply.github.com>
2022-12-16 22:55:38 +0100

    Update tests.py

--- decide/voting/tests.py
+++ decide/voting/tests.py
@@ -595,99 +595,99 @@ class TestCrud(BaseTestCase):
         self.assertTemplateNotUsed('voting_list.html')
 
     def test_show_negative(self):
-         url = 'voting/2'
-         response = self.client.get(url)
-         self.assertEqual(response.status_code, 404)
-         self.assertTemplateNotUsed('voting_details.html')
+        url = 'voting/2'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+        self.assertTemplateNotUsed('voting_details.html')
 
     def test_show_positive(self):
-         q = Question(desc='test question')
-         q.save()
-         for i in range(5):
-            opt = QuestionOption(question=q, option='option {}'.format(i+1))
-            opt.save()
-         v = Voting.objects.create(
-            desc='Hablamos del chocolate',
+        q = Question(desc='test question')
+        q.save()
+        for i in range(5):
+           opt = QuestionOption(question=q, option='option {}'.format(i+1))
+           opt.save()
+        v = Voting.objects.create(
+           desc='Hablamos del chocolate',
         )
-         a, _ = Auth.objects.get_or_create(url=settings.BASEURL,
+        a, _ = Auth.objects.get_or_create(url=settings.BASEURL,
                                           defaults={'me': True, 'name': 'test auth'})
-         a.save()
-         v.auths.add(a)
-         v.question.add(q)
-         response = self.client.get(f'/voting/{v.id}')
-         self.assertNotEqual(response.status_code, 404)
-         self.assertTemplateUsed('voting_details.html')
+        a.save()
+        v.auths.add(a)
+        v.question.add(q)
+        response = self.client.get(f'/voting/{v.id}')
+        self.assertNotEqual(response.status_code, 404)
+        self.assertTemplateUsed('voting_details.html')
 
     def test_update_positive(self):
-         q = Question(desc='test question')
-         q.save()
-         for i in range(5):
-            opt = QuestionOption(question=q, option='option {}'.format(i+1))
-            opt.save()
-         v = Voting.objects.create(
-            desc='Hablamos del chocolate',
+        q = Question(desc='test question')
+        q.save()
+        for i in range(5):
+           opt = QuestionOption(question=q, option='option {}'.format(i+1))
+           opt.save()
+        v = Voting.objects.create(
+           desc='Hablamos del chocolate',
         )
-         a, _ = Auth.objects.get_or_create(url=settings.BASEURL,
+        a, _ = Auth.objects.get_or_create(url=settings.BASEURL,
                                           defaults={'me': True, 'name': 'test auth'})
-         a.save()
-         v.auths.add(a)
-         v.question.add(q)
-         response = self.client.post(f'/voting/{v.id}', {
-             'name' : 'test',
-         })
-         self.assertEqual(response.status_code, 301)
-         self.assertTemplateUsed('voting_details.html')
+        a.save()
+        v.auths.add(a)
+        v.question.add(q)
+        response = self.client.post(f'/voting/{v.id}', {
+            'name' : 'test',
+        })
+        self.assertEqual(response.status_code, 301)
+        self.assertTemplateUsed('voting_details.html')
 
     def test_update_negative(self):
-         q = Question(desc='test question')
-         q.save()
-         for i in range(5):
-            opt = QuestionOption(question=q, option='option {}'.format(i+1))
-            opt.save()
-         v = Voting.objects.create(
-            desc='Hablamos del chocolate',
+        q = Question(desc='test question')
+        q.save()
+        for i in range(5):
+           opt = QuestionOption(question=q, option='option {}'.format(i+1))
+           opt.save()
+        v = Voting.objects.create(
+           desc='Hablamos del chocolate',
         )
-         a, _ = Auth.objects.get_or_create(url=settings.BASEURL,
+        a, _ = Auth.objects.get_or_create(url=settings.BASEURL,
                                           defaults={'me': True, 'name': 'test auth'})
-         a.save()
-         v.auths.add(a)
-         v.question.add(q)
-         url = 'voting/2'
-         response = self.client.post(url, {
-             'name' : 'test',
-         })
-         self.assertEqual(response.status_code, 404)
-         self.assertTemplateNotUsed('voting_details.html')
+        a.save()
+        v.auths.add(a)
+        v.question.add(q)
+        url = 'voting/2'
+        response = self.client.post(url, {
+            'name' : 'test',
+        })
+        self.assertEqual(response.status_code, 404)
+        self.assertTemplateNotUsed('voting_details.html')
 
     def test_delete_positive(self):
-         q = Question(desc='test question')
-         q.save()
-         for i in range(5):
-            opt = QuestionOption(question=q, option='option {}'.format(i+1))
-            opt.save()
-         v = Voting.objects.create(
-            desc='Hablamos del chocolate',
+        q = Question(desc='test question')
+        q.save()
+        for i in range(5):
+           opt = QuestionOption(question=q, option='option {}'.format(i+1))
+           opt.save()
+        v = Voting.objects.create(
+           desc='Hablamos del chocolate',
         )
-         a, _ = Auth.objects.get_or_create(url=settings.BASEURL,
-                                          defaults={'me': True, 'name': 'test auth'})
-         a.save()
-         v.auths.add(a)
-         v.question.add(q)
-         v.delete()
-         self.assertTrue(Voting.objects.count() == 0)
+        a, _ = Auth.objects.get_or_create(url=settings.BASEURL,
+                                         defaults={'me': True, 'name': 'test auth'})
+        a.save()
+        v.auths.add(a)
+        v.question.add(q)
+        v.delete()
+        self.assertTrue(Voting.objects.count() == 0)
 
     def test_delete_negative(self):
-         q = Question(desc='test question')
-         q.save()
-         for i in range(5):
-            opt = QuestionOption(question=q, option='option {}'.format(i+1))
-            opt.save()
-         v = Voting.objects.create(
-            desc='Hablamos del chocolate',
+        q = Question(desc='test question')
+        q.save()
+        for i in range(5):
+           opt = QuestionOption(question=q, option='option {}'.format(i+1))
+           opt.save()
+        v = Voting.objects.create(
+           desc='Hablamos del chocolate',
         )
-         a, _ = Auth.objects.get_or_create(url=settings.BASEURL,
-                                          defaults={'me': True, 'name': 'test auth'})
-         a.save()
-         v.auths.add(a)
-         v.question.add(q)
-         self.assertTrue(Voting.objects.count() != 0)
+        a, _ = Auth.objects.get_or_create(url=settings.BASEURL,
+                                         defaults={'me': True, 'name': 'test auth'})
+        a.save()
+        v.auths.add(a)
+        v.question.add(q)
+        self.assertTrue(Voting.objects.count() != 0)
